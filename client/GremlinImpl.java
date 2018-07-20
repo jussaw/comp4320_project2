b3ce887db1184b90da7672946034c74f59419c4b
BuildTools <unconfigured@null.spigotmc.org>
2018-07-20 16:44:37 -0500

    add skeleton code

--- client/GremlinImpl.java
+++ client/GremlinImpl.java
@@ -8,54 +8,11 @@ public class GremlinImpl implements IGremlin {
 
 	}
 	public DatagramPacket corruptPacket(DatagramPacket packet, float corrupProb, float lossProb) {
-		float newLossProb = lossProb + corrupProb;
 
-		Random rand = new Random();
-		float randCorruptDamageCorrect = rand.nextFloat();
-		DatagramPacket corruptedPacket = packet;
-
-		if(randCorruptDamageCorrect < corrupProb) {
-			float randHowCorruptPacket = rand.nextFloat();
-			if(randHowCorruptPacket >= 0 && randHowCorruptPacket < .5) {
-				corruptedPacket = makeCorruption(packet, 1);
-			}
-			if(randHowCorruptPacket >= .5 && randHowCorruptPacket < .8) {
-				corruptedPacket = makeCorruption(packet, 2);
-			}
-			if(randHowCorruptPacket >= .8 && randHowCorruptPacket <= 1) {
-				corruptedPacket = makeCorruption(packet, 3);
-			}
-		}
-		if(randCorruptDamageCorrect >= corrupProb && randCorruptDamageCorrect < newLossProb) {
-			//Code to not send ack to server
-		}
-		return corruptedPacket;
+		return new DatagramPacket(new byte[], 0);
 	}
 
 	public DatagramPacket makeCorruption(DatagramPacket packetIn, int numOfPacketsToCorrupt) {
-		Random rand = new Random();
-		int iter = numOfPAckets;
-		int bitToCorrupt;
-		int[] alreadyCorrupted = {500, 500, 500};
-		byte[] corruptedBytes = packetIn.getData();
-		boolean alreadyCorruptedBool = false;
-		DatagramPacket corruptedPacket = packetIn;
 
-		while(iter > 0) {
-			alreadyCorruptedBool = false;
-			for (int num : alreadyCorrupted) {
-				if(num < 500){
-					alreadyCorruptedBool = true;
-				}
-			}
-
-			if(!alreadyCorruptedBool){
-				bitToCorrupt = rand.nextInt(256);
-				alreadyCorrupted[iter - 1] = bitToCorrupt;
-				corruptedBytes[bitToCorrupt]++;
-				corruptedPacket.setData(corruptedBytes);
-				iter--;
-			}
-		}
-		return corruptedPacket;
+		return new DatagramPacket(new byte[], 0);
 	}
