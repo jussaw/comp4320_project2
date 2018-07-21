470526f9ef136d1173503c7af0e84bd973e6a176
anb0037 <anb0037@auburn.edu>
2018-07-21 16:33:14 -0500

    error detection fixes

--- client/ErrorDetectionImpl.java
+++ client/ErrorDetectionImpl.java
@@ -1,12 +1,37 @@
 package client;
 import java.net.DatagramPacket;
+
 public class ErrorDetectionImpl implements IErrorDetection {
-    // checks the validity of a packet given a hash/checksum
-    ErrorDetectionImpl() {
-        this.numberOfCorruptedPackets = 0;
-    }
-    public int numberOfCorruptedPackets;
-    public boolean detectErrors(DatagramPacket packet) {
-        return false;
-    }
+	public boolean detectErrors(DatagramPacket packet) {
+		byte[] data = getData(packet);
+		int receivedChecksum = getReceivedChecksum(packet);
+		int calculatedChecksum = 0;
+		for (byte b : data) {
+			calculatedChecksum += new Byte(b).intValue();
+		}
+		System.out.println("Calculated checksum: " + calculatedChecksum);
+		System.out.println("Parsed checksum: " + getReceivedChecksum(packet));
+		return calculatedChecksum != getReceivedChecksum(packet);
+	}
+	public byte[] getData(DatagramPacket packet) {		
+		String headersAndData = new String(packet.getData());
+		String data = headersAndData.substring(headersAndData.indexOf("\r\n\r\n") + 4);
+		return data.getBytes();
+	}
+	public int getReceivedChecksum(DatagramPacket packet) {
+		try {
+			String headersAndData = new String(packet.getData());
+			String headers = headersAndData.substring(0, headersAndData.indexOf("\r\n\r\n"));
+			String headersArr[] = headers.split("\r\n");
+			for (String header : headersArr) {
+				if (header.split(" ")[0].equals("Checksum:")) {
+					return Integer.parseInt(header.split(" ")[1]);
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Unable to parse checksum!");
+			return 0;
+		}
+		return 0;
+	}
 }
--- client/IErrorDetection.java
+++ client/IErrorDetection.java
@@ -1,6 +1,6 @@
 package client;
 import java.net.DatagramPacket;
 public interface IErrorDetection {
-	// checks the validity of a packet given a hash/checksum
+	// checks the validity of a packet given a checksum
 	boolean detectErrors(DatagramPacket packet);
 }
