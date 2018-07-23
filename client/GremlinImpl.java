b684ab301e9201650d629f50c4968b5c0449db59
Justin Sawyer <jtsaw36@gmail.com>
2018-07-23 12:05:11 -0500

    Changes

--- client/GremlinImpl.java
+++ client/GremlinImpl.java
@@ -8,24 +8,37 @@ public class GremlinImpl implements IGremlin {
 
 	}
 	public DatagramPacket corruptPacket(DatagramPacket packet, float corrupProb, float lossProb) {
+		// Sets probability of loss to be between probability of corruption and 1.
 		float newLossProb = lossProb + corrupProb;
 
+		/* Makes new Random object
+		 * Makes new float randCorruptDamageCorrect which will determine whether to
+		 * corrupt the packet, lose the packet, or do nothing to the packet.
+		 * Makes a new DatagramPacket which will store the corrupted version of packet.
+		 */
 		Random rand = new Random();
 		float randCorruptDamageCorrect = rand.nextFloat();
 		DatagramPacket corruptedPacket = packet;
 
+		// If statement to corrupt packet
 		if(randCorruptDamageCorrect < corrupProb) {
+			// Makes float to check check how many bits to corrupt.
 			float randHowCorruptPacket = rand.nextFloat();
+			// Corrupt 1 bit.
 			if(randHowCorruptPacket >= 0 && randHowCorruptPacket < .5) {
 				corruptedPacket = makeCorruption(packet, 1);
 			}
+			// Corrupt 2 bits.
 			if(randHowCorruptPacket >= .5 && randHowCorruptPacket < .8) {
 				corruptedPacket = makeCorruption(packet, 2);
 			}
+			// Corrupt 3 bits.
 			if(randHowCorruptPacket >= .8 && randHowCorruptPacket <= 1) {
 				corruptedPacket = makeCorruption(packet, 3);
 			}
 		}
+
+		//If statement to allow packet loss to occur
 		if(randCorruptDamageCorrect >= corrupProb && randCorruptDamageCorrect < newLossProb) {
 			return null;
 		}
